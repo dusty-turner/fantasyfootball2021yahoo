bc0a48c52f1172ea6ea3554cb4adf07d864b591a
Turner <1266213561121004@mil>
2021-11-09 11:33:03 -0500

    commit dashboard

--- call_script.R
+++ call_script.R
@@ -1,8 +1,8 @@
 source("01_scripts/create_yahoo_token.R")
-yahoo_token <- httr::oauth2.0_access_token(yahoo,myapp,code="g55f994")
+yahoo_token <- httr::oauth2.0_access_token(yahoo,myapp,code="pk7ka2f")
 save(yahoo_token,file="yahoo_token.Rdata")
 
-week_num <- 7
+week_num <- 8
 
 rmarkdown::render("GOAT_Dashboard.Rmd", 
                   params = list(week_num = week_num)
